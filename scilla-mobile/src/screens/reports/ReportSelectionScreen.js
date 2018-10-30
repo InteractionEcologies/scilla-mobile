05635e0751384cfd736d07096eac3701f2cdbf5b
sprite728 <chuanche@umich.edu>
2018-10-30 14:18:39 -0400

    Revised ReportSelectionScreen
    Former-commit-id: a9fccc37627824e6fce95c36458a89085fdc5f65

--- scilla-mobile/src/screens/reports/ReportSelectionScreen.js
+++ scilla-mobile/src/screens/reports/ReportSelectionScreen.js
@@ -16,6 +16,7 @@ import type {
 } from "../../libs/intecojs"
 
 const appState = new AppState();
+const DAILY_EVALUATION_MEASUREMENT_TYPE = "Daily Evaluation"
 
 type State = {
   selectedDate: string, 
@@ -60,7 +61,10 @@ export default class ReportSelectionScreen extends React.Component<any, State> {
       let regimen = await appState.getLatestRegimen();
       if(regimen) {
         this.setState({
-          trackedMeasurementTypes: [...regimen.getTrackedMeasurementTypes(),'Daily Evaluation'],
+          trackedMeasurementTypes: [
+            ...regimen.getTrackedMeasurementTypes(),
+            DAILY_EVALUATION_MEASUREMENT_TYPE
+          ],
         });
       }
     } catch (e) {
@@ -68,43 +72,20 @@ export default class ReportSelectionScreen extends React.Component<any, State> {
     }
   }
 
-  _goToMeasurementScreen = (type:string) =>{
+  _goToMeasurementScreen = (type: string) => {
     this.props.navigation.navigate(
       ScreenNames.ReportMeasurement, 
       { trackedMeasurementType: type }
     )
   }
 
-  _goToDailyEvaluationScreen = (date: string) =>{
+  _goToDailyEvaluationScreen = (date: string) => {
     this.props.navigation.navigate(
       ScreenNames.ReportDailyEvaluation,
       { selectedDate: date }
     )
   }
 
-  renderMeasurementTypeOptions = ():any =>{
-    if(this.state.trackedMeasurementTypes){
-      return this.state.trackedMeasurementTypes.map((type: string, i: number):any=>{
-          return (
-          <Button 
-              key={i}
-              style={styles.optionButton}
-              bordered={true}
-              block
-              onPress={(type==='Daily Evaluation')? 
-                        ()=>this._goToDailyEvaluationScreen(this.state.selectedDate)
-                        :()=>this._goToMeasurementScreen(type) }
-          >
-              <AppText>{type}</AppText>
-          </Button>
-          );
-      })
-    }else{
-      return(
-        <AppText>No tracked measurement being set</AppText>
-      );
-    }
-  }
   onDayPressed = (day: XDate) => {
     this.setState({
       selectedDate: day.dateString
@@ -118,29 +99,79 @@ export default class ReportSelectionScreen extends React.Component<any, State> {
         selected: true
       }
     }
-      return(
-        <Container style={styles.container}>
-          <View style={styles.header}>
-              <OneWeekCalendar style={styles.calendarView}
-                current={this.state.selectedDate}
-                onDayPress={this.onDayPressed}
-                markedDates={markedDates}
-              />
-          </View>
-          <ScrollView>
-            <Content contentContainerStyle={styles.content}>
-              <Card style={styles.selectionCard}>
-                <CardItem style = {styles.cardItems} bordered>
-                  <Title style={styles.titleText}>Report your experience</Title>   
-                </CardItem>
-                <CardItem style={styles.cardButtons}>
-                  {this.renderMeasurementTypeOptions()}    
-                </CardItem>
-              </Card>              
-            </Content>
-          </ScrollView>
-        </Container>
-      );
-    }
 
+    return(
+      <Container style={styles.container}>
+        <View style={styles.header}>
+            <OneWeekCalendar style={styles.calendarView}
+              current={this.state.selectedDate}
+              onDayPress={this.onDayPressed}
+              markedDates={markedDates}
+            />
+        </View>
+        <ScrollView>
+          <Content contentContainerStyle={styles.content}>
+              {this.renderReportCard()}
+          </Content>
+        </ScrollView>
+      </Container>
+    );
+  }
+
+  renderReportCard = () => {
+    if(this._regimenExist()) {
+      return (
+        <Card style={styles.selectionCard}>
+          <CardItem style = {styles.cardItems} bordered>
+            <Title style={styles.titleText}>Report your experience</Title>   
+          </CardItem>
+          <CardItem style={styles.cardButtons}>
+            {this.renderMeasurementTypeOptions()}    
+          </CardItem>
+        </Card>
+      )
+    } else {
+      return (
+        <Card style={styles.selectionCard}>
+          <CardItem style = {styles.cardItems} bordered>
+            <Title>To report your experience, you have to first setup a regimen.</Title>   
+          </CardItem>
+        </Card>
+      )
+    }
+  }
+
+  _regimenExist = () => {
+    // We will attach a daily evaluation to the options 
+    // displayed to the user if a regimen exist. Thus if there 
+    // is no reporting option that means a regimen does not exist. 
+    if(this.state.trackedMeasurementTypes.length > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  renderMeasurementTypeOptions = () => {
+    let optionButtons = _.map(
+      this.state.trackedMeasurementTypes,
+      (type: string, i: number) => {
+        console.log(type);
+        return (
+          <Button 
+              key={i}
+              style={styles.optionButton}
+              bordered={true}
+              block
+              onPress={ (type === DAILY_EVALUATION_MEASUREMENT_TYPE) ? 
+                        () => this._goToDailyEvaluationScreen(this.state.selectedDate)
+                        :() => this._goToMeasurementScreen(type) }
+          >
+            <AppText>{type}</AppText>
+          </Button>
+        );
+      })
+    
+    return optionButtons;
+  }
 }
